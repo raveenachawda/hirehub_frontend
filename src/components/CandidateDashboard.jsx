fac86d506a87eb0780ba46c2d3d97ecf7f6eb3f3
raveenachawda <raveenachawda0228@gmail.com>
2025-05-27 14:36:25 +0530

    edit

--- src/components/CandidateDashboard.jsx
+++ src/components/CandidateDashboard.jsx
@@ -4,12 +4,12 @@ import Footer from "./shared/Footer";
 import Slidebar from "./Slidebar";
 import { Outlet } from "react-router-dom";
 import CandidateMainContent from "./CandidateMainContent";
-
+import candidatedashboardImage from "../assets/candidatedashboardImage.jpg";
 const CandidateDashboard = () => {
   return (
     <div
       className="min-h-screen bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('/src/assets/candidatedashboardImage.jpg')" }}
+      style={{ backgroundImage: `url(${candidatedashboardImage})` }}
     >
       <Navbar />
       <div className="flex flex-col lg:flex-row">
